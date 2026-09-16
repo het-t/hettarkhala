af37959f9d35b41018beaad0f1320c096548525c
Het Tarkhala <89692764+het-t@users.noreply.github.com>
2026-09-16 09:56:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/hettarkhala/",
-
   tanstackStart: {
     server: { entry: "server" },
 
